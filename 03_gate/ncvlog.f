0f234df55edd1afef11d6e5e8e910a933cd5dc6d
Henry Juan <legendboy325@gmail.com>
2023-05-17 22:29:48 +0800

    Add files via upload

---
+++ 03_gate/ncvlog.f
@@ -0,0 +1,9 @@
+LASER_syn.v
+-timescale 1ns/10ps
+-sv tb.sv
++define+P6
+#-v path for the tsmc13_neg.v
+-v ~/IC_CONTEST/CBDK_IC_Contest_v2.5/Verilog/tsmc13_neg.v 
++define+SDF 
++ncmaxdelays
++debug
